f248437ce0b331be0c7d3946754d2706209bd331
MaxenceBossin <maxbossin25@gmail.com>
2022-10-28 17:07:10 +0200

    branch

--- src/Entity/Dumpster.php
+++ src/Entity/Dumpster.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DumpsterRepository::class)]
+//#[ApiResource(security: "is_granted('ROLE_USER')")]
 #[ApiResource()]
 class Dumpster
 {
--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\MessageRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\MessageRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
+#[ApiResource()]
 class Message
 {
     #[ORM\Id]
